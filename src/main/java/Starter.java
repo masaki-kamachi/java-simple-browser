efdd82a6bd33ca6c0434cbdf6451c2a8ae66fff5
masaki-kamachi <masaki.kamachi@bizreach.co.jp>
2017-02-28 17:34:16 +0900

    起動

---
+++ src/main/java/Starter.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+/**
+ * ブラウザ起動用のクラスです。
+ * @author masaki.kamachi
+ */
+public class Starter {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setSize(1000, 1000);
+        frame.setTitle("Java Simple Browser");
+        frame.pack();
+    }
+
+}
